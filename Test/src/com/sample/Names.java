e94aca5c925bf13c208a34dee5638684fffc224a
N9JOSE <N9JOSE@NA03OSDVP00167.aceins.com>
2017-09-27 05:56:01 -0400

    Checking it into develop branch

---
+++ Test/src/com/sample/Names.java
@@ -0,0 +1,7 @@
+package com.sample;
+
+public enum Names {
+	
+	String name1 = "Nancy";
+
+}
---
+++ Test/src/com/sample/TestMendixDate.java
@@ -0,0 +1,33 @@
+package com.sample;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import powergen.actions.J_CalculateDateInDays;
+
+public class TestMendixDate {
+	
+	
+	public static void main(String[] args) throws Exception{
+		
+		/*J_CalculateDateInDays jC = new J_CalculateDateInDays(42735.00);
+		jC.executeAction();*/
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");		
+		Date date = sdf.parse("12/12/2016");
+		System.out.println(date);
+		
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd MM yyyy");		
+		String newDateString = sdf2.format(date);
+		System.out.println(newDateString);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
